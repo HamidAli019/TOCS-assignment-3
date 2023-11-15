e08f10061133fe5c2b2869967ca43d72a8d47610
Hamid Ali <hamidali.p860@gmail.com>
2023-11-15 11:20:02 +0500

    I have done table of 2

--- test.java
+++ test.java
@@ -1,7 +1,9 @@
 public class test {
     
-    public static void main(String[] args)
-    {
-        System.out.println("hello world");
-    }
+    public static void main(String[] args){
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println("2"+" "+"X"+" "+i+" "+"="+" "+i*2);
+        }
+    };
 }
